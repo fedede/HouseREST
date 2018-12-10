d26aacdce6e772b3f52c8a4fd02187d1430310ef
Adri El Guapo <kugoad@openmailbox.org>
2018-12-10 12:32:24 +0100

    Fix search

--- src/main/java/com/tiwbnb/api/domains/HouseDAO.java
+++ src/main/java/com/tiwbnb/api/domains/HouseDAO.java
@@ -11,10 +11,10 @@ public interface HouseDAO extends CrudRepository<House, Long>{
 	public Optional<House> findById(Long id);
 	public List<House> findByOwnerId(Long id);
 	
-	@Query(value = "SELECT * FROM house h WHERE (?1 = null OR city = ?1) " 
-			+ "AND (?2 = null OR price > ?2) AND (?3 = null OR price < ?3) "
-			+ "AND (?4 = null OR max_guests > ?4) AND (?5 = null OR shared = ?5) "
-			+ "AND (?6 = null OR start_date <= ?6) AND (?7 = null OR end_date >= ?7)", 
+	@Query(value = "SELECT * FROM house h WHERE (?1 IS null OR city = ?1) " 
+			+ "AND (?2 IS null OR price > ?2) AND (?3 IS null OR price < ?3) "
+			+ "AND (?4 IS null OR max_guests > ?4) AND (?5 IS null OR shared = ?5) "
+			+ "AND (?6 IS null OR start_date <= ?6) AND (?7 IS null OR end_date >= ?7)", 
 			nativeQuery = true)
 	public List<House> find(String city, Float minPrice, Float maxPrice, Integer maxGuests, 
 			Boolean shared, Date startDate, Date endDate);
